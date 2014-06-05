1f86a22e077d3871f0dac7b4cc89871832158814
puneetrastogi90 <puneetrastogi90@gmail.com>
2014-06-05 14:55:24 +0530

    no message

--- my-app/www/js/index.js
+++ my-app/www/js/index.js
@@ -19,6 +19,12 @@
 var app = {
     // Application Constructor
     initialize: function() {
+    	navigator.notification.alert(
+                'inside initialize',  // message
+                function(){},         // callback
+                'title',            // title
+                'ok'                  // buttonName
+            );
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -26,6 +32,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+    	
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -36,14 +43,134 @@ var app = {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+    receivedEvent: function() {
+       /* var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id);*/
+    	
+    	navigator.notification.alert(
+                'inside received event',  // message
+                function(){},         // callback
+                'title',            // title
+                'ok'                  // buttonName
+            );
+    	getFileSystem();
+    	showImage();
+    	var numDirs = 0;
+    	var numFiles = 0;
+    	var ImageFilePath = new Array();
+    	
+    	GetAllImageFromSD();
+    	
     }
+    function GetAllImageFromSD() {
+    	navigator.notification.alert(
+                'inside GetAllImageFromSD',  // message
+                function(){},         // callback
+                'title',            // title
+                'ok'                  // buttonName
+            );
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
+       
+    }
+
+    function onFileSystemSuccess(fileSystem) {
+    	navigator.notification.alert(
+                'inside onFileSystemSuccess',  // message
+                function(){},         // callback
+                'title',            // title
+                'ok'                  // buttonName
+            );
+        fileSystem.root.getDirectory("/sdcard", { create: false, exclusive: false }, getDirSuccess, fail);
+    }
+
+    function getDirSuccess(dirEntry) {
+    	navigator.notification.alert(
+                'inside getDirSuccess',  // message
+                function(){},         // callback
+                'title',            // title
+                'ok'                  // buttonName
+            );
+        var directoryReader = dirEntry.createReader();
+
+        // Get a list of all the entries in the directory
+        directoryReader.readEntries(readerSuccess, fail);
+    }
+
+
+    var readerTimeout = null, millisecondsBetweenReadSuccess = 1000;
+
+    function readerSuccess(entries) {
+        String.prototype.startsWith = function (str)
+        { return (this.match("^" + str) == str) }
+
+        var i = 0, len = entries.length;
+        for (; i < len; i++) {
+            if (entries[i].isFile) {
+                if ((entries[i].name.indexOf(".jpeg") != -1) || (entries[i].name.indexOf(".png") != -1) || (entries[i].name.indexOf(".jpg") != -1)) {
+                    var fileName = entries[i].name;
+                    if (!fileName.startsWith(".")) {
+                        numFiles++;
+                        ImageFilePath.push(entries[i].fullPath)
+                        //  console.log("file "+entries[i].fullPath)
+                    }
+                }
+
+            } else if (entries[i].isDirectory) {
+                numDirs++;
+                // console.log("directory "+entries[i].name)
+                var dirName = entries[i].name;
+                if (!dirName.startsWith(".")) {
+                    getDirSuccess(entries[i]);
+                }
+            }
+            if (readerTimeout) {
+                window.clearTimeout(readerTimeout);
+            }
+        }
+        if (readerTimeout) {
+            window.clearTimeout(readerTimeout);
+        }
+        readerTimeout = window.setTimeout(weAreDone, millisecondsBetweenReadSuccess);
+    }
+
+    function weAreDone() {
+
+    	navigator.notification.alert(
+                'inside weAreDone',  // message
+                function(){},         // callback
+                'title',            // title
+                'ok'                  // buttonName
+            );
+    console.log("numDirs " + numDirs);
+    console.log("numFiles " + numFiles);
+
+    var a = "";
+
+    var GalleryImageTag = "";
+    GalleryImageTag = "<div class='spacingbox'></div> ";
+
+    for (var j = 0; j < ImageFilePath.length; j++) {
+        GalleryImageTag += "<div class='divgallery divcolor'>"
+        var ChkId = "chk" + j;
+        GalleryImageTag += "<img class='imggallery' id='" + j + "' src=' " + ImageFilePath[j] + "'alt=''/>";
+        GalleryImageTag += "<input id='" + ChkId + "' name='" + ChkId + "' type='checkbox' value='" + ImageFilePath[j] + "' class='allignchk'>";
+        GalleryImageTag += "</div>";
+
+    }
+    $('#ImageContainer').html('');
+    $('#ImageContainer').append(GalleryImageTag);
+    $('#GalleryView').popup("open");
+    numDirs = 0;
+    numFiles = 0;
+
+    ImageFilePath = [];
+    console.log(GalleryImageTag);
+    }
+    	
 };
